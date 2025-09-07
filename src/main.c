94c858676dac73f370aedf4348784f8c6d1ffd43
OngakuKun <marcellehmkuhl@outlook.de>
2025-09-07 02:07:22 +0200

    Refactor main.c to generate random numbers and add them

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
+#include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "typedef.h"
 
 int main() {
-  printf("Hello, World!\n");
-  return 0;
+
+    srand((unsigned)time(NULL));
+
+    // Generate two random numbers using /dev/urandom
+    int8 a = rand() % ((INT8_MAX / 2) + 1);
+    int8 b = rand() % ((INT8_MAX / 2) + 1);
+
+    printf("Hello, World! The random seed is: %" PRId8 "\n", a + b);
+    return 0;
 }
